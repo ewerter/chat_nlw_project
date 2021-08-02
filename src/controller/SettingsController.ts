830fa436328fc9c09fe96ca5d350cb895265b97e
ewerter <ewertontertuliano@gmail.com>
2021-08-02 12:44:36 -0400

    removing business rule from controller and create a service to manage that

--- src/controller/SettingsController.ts
+++ src/controller/SettingsController.ts
@@ -1,20 +1,18 @@
 import{ Request, Response} from "express";
 import{getCustomRepository} from "typeorm";
 import{SettingsRepository} from "../repositories/SettingsRepository";
+import {SettingsService} from "../Services/SettingsService";
 
 
 class SettingsController {
 
     async create(request: Request, response: Response){
         const {chat, username} = request.body;
-
-        const settingsRepository = getCustomRepository(SettingsRepository);
-    
-        const settings = settingsRepository.create({
-            chat,
-            username,
-        })
-        await settingsRepository.save(settings);
+        
+        const settingsService = new SettingsService();
+        
+        const settings = await settingsService.create({chat, username});
+        
         return response.json(settings);
     }
 }
